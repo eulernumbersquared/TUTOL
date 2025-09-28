7f2daa807fcaa72e5fe0dea954f9253ba2f2b89b
eulernumbersquared <lalinperera2000@gmail.com>
2025-09-28 21:09:02 +0300

    test

--- js/layers.js
+++ js/layers.js
@@ -1,10 +1,10 @@
 addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+    name: "seeds", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "s", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(0),
+		points: new Decimal(10^30),
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
--- js/mod.js
+++ js/mod.js
@@ -1,6 +1,6 @@
 let modInfo = {
-	name: "The ??? Tree",
-	author: "nobody",
+	name: "The upgrade tree of life",
+	author: "e^2",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
@@ -12,8 +12,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "we're barack",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
