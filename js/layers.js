08fd6645372f9673b8d3798228f4373133a585c3
eulernumbersquared <lalinperera2000@gmail.com>
2025-09-29 20:07:14 +0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -8,8 +8,8 @@ addLayer("p", {
     }},
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    resource: "Seeds", // Name of prestige currency
+    baseResource: "Leaves", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
