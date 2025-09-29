718647f4376aa19d917c8d47d341e8da6af7eb3a
eulernumbersquared <lalinperera2000@gmail.com>
2025-09-29 20:32:51 +0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -46,6 +46,7 @@ function getPointGen() {
 	if (hasUpgrade('p', 11)) gain = gain.times(5)
 	if (hasUpgrade('p', 12)) gain = gain.times(upgradeEffect('p', 12))
 	if (hasUpgrade('p', 14)) gain = gain.times(upgradeEffect('p', 14))
+	if (hasUpgrade('p', 15)) gain = gain.times(upgradeEffect('p', 15))
 	
 	return gain
 }
