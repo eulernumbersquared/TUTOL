a25432ef159d1537887df5ea35caede059f32958
eulernumbersquared <lalinperera2000@gmail.com>
2025-09-29 20:06:31 +0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
-addLayer("p", {
-    name: "prestige points", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "p", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("S", {
+    name: "Seeds", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -23,7 +23,7 @@ addLayer("p", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "S", description: "S: Decomplize for some seeds", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
     layerShown(){return true},
         upgrades: {
