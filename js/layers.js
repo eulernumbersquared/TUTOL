7568856b8476125036d1faf8888da702f95bd438
eulernumbersquared <lalinperera2000@gmail.com>
2025-09-30 19:08:52 +0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -124,7 +124,20 @@ addLayer("F", {
     layerShown() { return true },          // Returns a bool for if this layer's node should be visible in the tree.
 
     upgrades: {
-        // Look in the upgrades docs to see what goes here!
+       buyables: {
+    11: {
+        cost(x) { return new Decimal(1).mul(x) },
+        display() { return "Blah" },
+        canAfford() { return player[this.layer].points.gte(this.cost()) },
+        buy() {
+            player[this.layer].points = player[this.layer].points.sub(this.cost())
+            setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
+        },
+        etc
     },
+    etc
+}
+    },
+    
     
 })
