3bd664b79a856aceeda08571f80c7135d81e7281
eulernumbersquared <lalinperera2000@gmail.com>
2025-09-30 19:35:45 +0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -133,7 +133,7 @@ buyables: {
     11: {
         cost(x) { return new Decimal(1).mul(x) },
         display() { return "Blah" },
-        canAfford() { return player[this.layer].points.gte(this.cost()) },
+        canAfford() { return player.points.gte(this.cost()) },
         buy() {
             player[this.layer].points = player[this.layer].points.sub(this.cost())
             setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
