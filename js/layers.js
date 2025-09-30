00e300da3825a1a51e1da5b58adba72fd7b76de1
eulernumbersquared <lalinperera2000@gmail.com>
2025-09-30 18:59:35 +0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -106,7 +106,7 @@ addLayer("F", {
     row: 1,                                 // The row this layer is on (0 is the first row).
 
     baseResource: "Seeds",                 // The name of the resource your prestige gain is based on.
-    baseAmount() { return player.points },  // A function to return the current amount of baseResource.
+    baseAmount() { return player.p.Seeds },  // A function to return the current amount of baseResource.
 
     requires: new Decimal(10),              // The amount of the base needed to  gain 1 of the prestige currency.
                                             // Also the amount required to unlock the layer.
