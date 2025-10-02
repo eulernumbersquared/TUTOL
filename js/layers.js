875d3831711019249f9a58fe2ad99f988796a1af
eulernumbersquared <lalinperera2000@gmail.com>
2025-10-02 19:44:56 +0300

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -135,6 +135,23 @@ addLayer("F", {
         
 
     },
+    buyables: {
+    11: {
+        cost(x) { return new Decimal(1).mul(x) },
+        display() { return "Boost leaves by 2 compounding" },
+        canAfford() { return player[this.layer].points.gte(this.cost()) },
+        buy() {
+            player[this.layer].points = player[this.layer].points.sub(this.cost())
+            setBuyableAmount(this.layer, this.id, getBuyableAmount(this.layer, this.id).add(1))
+        },
+        effect() {
+            player.points.times(2)
+        },
+        displayEffect() { this.effect},
+
+    },
+
+}
 
     
 })
